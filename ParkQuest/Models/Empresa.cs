68bcf1aa3913dd6cb2d453ad08dea90391a1f8a7
RamonBicalho <magalhaesramon4@gmail.com>
2022-10-22 21:16:57 -0300

    CRUD veiculos. EF core

---
+++ ParkQuest/Models/Empresa.cs
@@ -0,0 +1,11 @@
+﻿namespace Dominio.Entidades
+{
+    public class Empresa
+    {
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+        public string Responsavel { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
