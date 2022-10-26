43b5f542a2b38833096276263095c99e7fa654c6
RamonBicalho <magalhaesramon4@gmail.com>
2022-10-26 15:53:29 -0300

    Creat CRUD Estacionamento

--- ParkQuest/Migrations/20221026181503_M01.cs
+++ ParkQuest/Migrations/20221026181503_M01.cs
@@ -2,32 +2,38 @@
 
 namespace ParkQuest.Migrations
 {
-    public partial class M00 : Migration
+    public partial class M01 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+
             migrationBuilder.CreateTable(
-                name: "Usuario",
+                name: "Estacionamento",
                 columns: table => new
                 {
                     id = table.Column<int>(nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     nome = table.Column<string>(nullable: false),
-                    senha = table.Column<string>(nullable: false),
+                    endereco = table.Column<string>(nullable: false),
+                    vagas = table.Column<string>(nullable: false),
                     email = table.Column<string>(nullable: false),
-                    cpf = table.Column<string>(nullable: false)
+                    cnpj = table.Column<string>(nullable: false),
+                    cidade = table.Column<string>(nullable: false),
+                    valor = table.Column<string>(nullable: false),
+                    senha = table.Column<string>(nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Usuario", x => x.id);
+                    table.PrimaryKey("PK_Estacionamento", x => x.id);
                 });
-
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Usuario");
+                name: "Estacionamento");
+
+                }
         }
     }
-}
+
